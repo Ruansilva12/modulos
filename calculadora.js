2f14da4c079b82ca2cfc06a8cb017312601693f5
Ruan <ruancipriano2005@gmail.com>
2023-09-01 08:58:54 -0300

    Ruansilva12

--- calculadora.js
+++ calculadora.js
@@ -2,8 +2,11 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var multiplicar = require("./modulos/multiplicar");
 var dividir = require("./modulos/dividir");
+var mensagem = require("./modulos/mensagem");
+var nome = "Ruan";
 
-console.log(somar(20,90));
-console.log(media(20,90));
-console.log(multiplicar(20,90));
-console.log(dividir(20,90));
+console.log("A soma de x e y é: " + somar(20,90));
+console.log("A média de x e y é: " + media(20,90));
+console.log("A multiplicação de x e y é: " + multiplicar(20,90));
+console.log("A divisãpo de x e y é: " + dividir(20,90));
+console.log(mensagem(nome));
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem = function (nome) {
+    return "Olá, " + nome +". Seja bem vindo ao curso de Node.js"
+};
+module.exports = mensagem
